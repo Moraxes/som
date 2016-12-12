a97b8d5812601e51c3dcb13eb914f8412ba0c807
Maciej Śniegocki <m.w.sniegocki@gmail.com>
2016-12-13 00:17:15 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,125 @@
+#![feature(proc_macro)]
+
+// extern crate bincode;
+// extern crate byteorder;
+extern crate clap;
+extern crate ctrlc;
+extern crate image;
+// extern crate nalgebra;
+extern crate rand;
+// extern crate rayon;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json as sj;
+extern crate palette;
+
+mod som;
+mod program_args;
+
+use std::error::Error;
+use std::sync::Arc;
+use std::sync::atomic::{Ordering, AtomicBool};
+
+fn main() {
+  let args = program_args::get();
+
+  match args.subcommand() {
+    ("train-img", m) => train_img(m.unwrap()),
+    _ => unreachable!(),
+  }
+}
+
+fn gamma_expand(x: f32) -> f32 {
+  if x <= 0.04045 {
+    x / 12.92
+  } else {
+    ((x + 0.055) / 1.055).powf(2.4)
+  }
+}
+
+fn gamma_compress(x: f32) -> f32 {
+  if x <= 0.0031308 {
+    x * 12.92
+  } else {
+    1.055 * x.powf(1.0 / 2.4) - 0.055
+  }
+}
+
+fn sq_euclidean_dist(x: &[f32], y: &[f32]) -> f32 {
+  x.iter().zip(y).map(|(x, y)| x - y).map(|z| z * z).sum()
+}
+
+fn train_img<'a>(args: &clap::ArgMatches<'a>) {
+  use image::Pixel;
+  use rand::SeedableRng;
+
+  let img = match image::open(args.value_of("input").unwrap()) {
+    Ok(img) => img,
+    Err(err) => panic!("error while loading image: {}", err.description())
+  }.to_rgb();
+
+  let img_f32 = img.pixels().map(|p| {
+    use palette::*;
+
+    let rgb = p.to_rgb();
+    let r = gamma_expand(rgb[0] as f32 / 255.0);
+    let g = gamma_expand(rgb[1] as f32 / 255.0);
+    let b = gamma_expand(rgb[2] as f32 / 255.0);
+    let lab = Lab::from_rgb(Rgb::new(r, g, b));
+    vec![
+      lab.l,
+      lab.a,
+      lab.b,
+    ] 
+  }).collect::<Vec<Vec<f32>>>();
+
+  let conf: som::TrainConfig = {
+    use std::fs::File;
+    match File::open(args.value_of("config").unwrap()) {
+      Ok(file) => sj::from_reader(file).unwrap(),
+      Err(_) => panic!("no config file"),
+    }
+  };
+
+  let net_defn: som::RectSomDefn = {
+    use std::fs::File;
+    match File::open(args.value_of("net_defn").unwrap()) {
+      Ok(file) => sj::from_reader(file).unwrap(),
+      Err(_) => panic!("no network definition file"),
+    }
+  };
+
+  let mut rng = rand::XorShiftRng::from_seed(rand::random());
+  let mut net = som::RectSom::new_random(net_defn.width, net_defn.height, &vec![(0.0, 100.0), (-128.0, 127.0), (-128.0, 127.0)], &mut rng);
+
+  let learning = Arc::new(AtomicBool::new(true));
+  let l = learning.clone();
+  ctrlc::set_handler(move || {
+    l.store(false, Ordering::SeqCst);
+  });
+
+  som::train(&img_f32, &mut net, sq_euclidean_dist, &mut rng, &conf, learning);
+
+  let map_img = image::RgbImage::from_fn(net.width as u32, net.height as u32,
+      |x, y| {
+        use palette::*;
+        // let mut field = net.field(x as usize, y as usize).iter().map(|&c| gamma_compress(c) * 255.0).map(|c| c as u8);
+        // let r = field.next().unwrap();
+        // let g = field.next().unwrap();
+        // let b = field.next().unwrap();
+        let mut field = net.field(x as usize, y as usize).iter();
+        let rgb = Rgb::from_lab(Lab::new(*field.next().unwrap(), *field.next().unwrap(), *field.next().unwrap()));
+        image::Rgb {
+          data: [
+            (gamma_compress(rgb.red) * 255.0) as u8,
+            (gamma_compress(rgb.green) * 255.0) as u8,
+            (gamma_compress(rgb.blue) * 255.0) as u8,
+          ],
+        }
+      });
+  match map_img.save(args.value_of("image").unwrap()) {
+    Err(err) => panic!("failed to save image: {}", err.description()),
+    _ => {},
+  };
+}
---
+++ src/som.rs
@@ -0,0 +1,132 @@
+use rand::Rng;
+use std::sync::Arc;
+use std::sync::atomic::{Ordering, AtomicBool};
+
+#[derive(Serialize, Deserialize)]
+pub struct RectSomDefn {
+  pub width: usize,
+  pub height: usize,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct RectSom {
+  pub width: usize,
+  pub height: usize,
+  pub fields: Vec<Vec<f32>>,
+}
+
+impl RectSom {
+  pub fn new_random<R: Rng>(width: usize, height: usize, bounding_box: &[(f32, f32)], mut rng: &mut R) -> RectSom {
+    use rand::distributions::{Range, IndependentSample};
+    let distributions = bounding_box.iter().map(|&(min, max)| Range::new(min, max)).collect::<Vec<_>>();
+
+    RectSom {
+      width: width,
+      height: height,
+      fields: (0..width*height).map(|_|
+        distributions.iter().map(|&range| {
+          range.ind_sample(&mut rng)
+        }).collect()
+      ).collect()
+    }
+  }
+
+  pub fn field(&self, x: usize, y: usize) -> &[f32] {
+    &self.fields[x + self.width * y][..]
+  }
+
+  pub fn delinearize_coord(&self, idx: usize) -> (usize, usize) {
+    (idx % self.width, idx / self.height)
+  }
+
+  pub fn best_fitting_unit<Df: Copy + FnOnce(&[f32], &[f32]) -> f32>(&self, target: &[f32], dist: Df) -> (usize, usize) {
+    let mut min_idx = 0;
+    let mut min_dist = dist(&self.fields[min_idx][..], target);
+    for it in 1..self.fields.len() {
+      let d = dist(&self.fields[it][..], target);
+      if d < min_dist {
+        min_idx = it;
+        min_dist = d;
+      }
+    }
+    self.delinearize_coord(min_idx)
+  }
+
+  pub fn nudge_weights<Nf: Copy + FnOnce(isize, isize, f32) -> f32>(&mut self, at: (usize, usize), radius: f32, target: &[f32], neighbourhood_fn: Nf, rate: f32) -> f32 {
+    let (at_xi, at_yi) = (at.0 as isize, at.1 as isize);
+    let mut total_magnitude = 0f32;
+    for it in 0..self.fields.len() {
+      let (x, y) = self.delinearize_coord(it);
+      let (xi, yi) = (x as isize, y as isize);
+      let nudge_scale = neighbourhood_fn(xi - at_xi, yi - at_yi, radius);
+      let mut this_magnitude = 0f32;
+      for (jt, w) in self.fields[it].iter_mut().enumerate() {
+        let new_w = target[jt] * nudge_scale * rate + *w * (1.0 - nudge_scale * rate);
+        let diff = *w - new_w;
+        this_magnitude += diff * diff;
+        *w = new_w;
+      }
+      total_magnitude += this_magnitude.sqrt() / target.len() as f32;
+    }
+    total_magnitude / self.fields.len() as f32
+  }
+
+  pub fn apply_diff(&mut self, diff: &[Vec<f32>]) {
+    for it in 0..self.fields.len() {
+      for (jt, w) in self.fields[it].iter_mut().enumerate() {
+        *w += diff[it][jt];
+      }
+    }
+  }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct TrainConfig {
+  pub train_rate: f32,
+  pub stability_threshold: f32,
+  pub stability_duration: usize,
+  pub initial_radius: f32,
+  pub radius_decay: f32,
+  pub min_radius: Option<f32>,
+  pub max_epochs: usize,
+}
+
+pub fn gaussian(dx: isize, dy: isize, radius: f32) -> f32 {
+  let (dx, dy) = (dx as f32, dy as f32);
+  (-(dx*dx + dy*dy)/(2.0*radius*radius)).exp()
+}
+
+pub fn train<R: Rng, Df: Copy + FnOnce(&[f32], &[f32]) -> f32>(
+    examples: &[Vec<f32>],
+    som: &mut RectSom,
+    dist_fn: Df,
+    rng: &mut R,
+    conf: &TrainConfig,
+    stop_flag: Arc<AtomicBool>) {
+  let mut stability = 0usize;
+  let mut radius = conf.initial_radius;
+  let mut epoch = 0usize;
+
+  while stop_flag.load(Ordering::SeqCst) &&
+      epoch < conf.max_epochs &&
+      stability < conf.stability_duration {
+    epoch += 1;
+    if epoch % 10 == 0 {
+      println!("#{}", epoch);
+    }
+    let ex = &rng.choose(examples).unwrap()[..];
+    let bfu = som.best_fitting_unit(ex, dist_fn);
+    let diff = som.nudge_weights(bfu, radius, ex, gaussian, conf.train_rate);
+    if diff < conf.stability_threshold {
+      stability += 1;
+    } else {
+      stability = 0;
+    }
+    radius *= conf.radius_decay;
+    if let Some(min) = conf.min_radius {
+      if radius < min {
+        radius = min;
+      }
+    }
+  }
+}
